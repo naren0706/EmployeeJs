95737f5a69c9fcd9ae46eb57062007278641e141
naren0706 <narenthrakishhore@gmail.com>
2023-09-06 10:33:12 +0530

    [Narenthra Kishore] Add : new feature to get the employee's shift

--- empWages.js
+++ empWages.js
@@ -1,9 +1,33 @@
 const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HR = 4;
+const FULL_TIME_HR = 8;
+const WAGE_PER_HR = 20;
+
+//UC1
 
 let empCheck = Math.floor(Math.random() * 10) % 2;
+
 if (empCheck == IS_ABSENT) {
-    console.log("ABSENT");
+  console.log("Employee is Absent");
+} else {
+  console.log("Employee is present");
 }
-else {
-    console.log('PRESENT');
+
+//UC2
+
+let emphrs = 5;
+empCheck - (Math.floor(Math.random() * 10) % 3);
+switch (empCheck) {
+  case IS_PART_TIME:
+    emphrs = PART_TIME_HR;
+    break;
+  case IS_FULL_TIME:
+    emphrs = FULL_TIME_HR;
+    break;
+  case IS_ABSENT:
+    emphrs = IS_ABSENT;
 }
+let empWage = emphrs * WAGE_PER_HR;
+console.log(empWage);
